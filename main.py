c427c538555b5c06f5624d642fb4aaf28ae65141
qianshuuhan <1652532319@qq.com>
2025-09-13 17:58:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# 在这个文件里编写代码
+initial_weight = 60
+moon_ratio = 0.165
+yarly_gain = 0.5
+print("年份\t地球体重(kg)\t月球体重(kg)")
+for year in range(1,11):
+    earth_weight = initial_weight + yearly_gain * year
+    moon_weight = earth_weight * moon_ratio
+    print(f"{year}\t{earth_weight:.2f}\t\t{moon_weight:.2f}")
